4b97c3605a7ebd099ef305bb56d1f4d62ab3d116
Aw Gh <awgh@awgh.org>
2021-01-30 01:25:30 -0800

    Fixed the downstream issue. Now calculating the size correctly and sending down the maximum number of answers in each response.

--- dns/downstream.go
+++ dns/downstream.go
@@ -2,6 +2,7 @@ package dns
 
 import (
 	"fmt"
+	"math"
 	"net"
 	"time"
 
@@ -17,7 +18,6 @@ import (
 
 // WriteDownstream - Writes KCP data to the DNS Client via DNS Responses
 func (m *Module) WriteDownstream(buf []byte, size int) {
-	// serverMetrics.RawBytesOut(size)
 	b := make([]byte, size)
 	copy(b, buf[:size])
 	m.downstreamKCPData <- b
@@ -47,14 +47,17 @@ func (m *Module) handleDNS(w mdns.ResponseWriter, req *mdns.Msg) {
 		}
 	}
 
+	// scrub out the original name to save space, matching transaction ID is all you need anyway
+	msg.Question = make([]mdns.Question, 1)
+	msg.Question[0] = mdns.Question{Name: "mail.", Qtype: req.Question[0].Qtype, Qclass: mdns.ClassINET}
+
 	// fetch outbound data from kcp and load into response
 	var answers []mdns.RR
 	var item []byte
-	itemCount := 0
 
 	select {
 	case item = <-m.downstreamKCPData:
-		itemCount++
+
 		// base32 encode, then dotify / "DNS chop"
 		b32s, err := Dotify(item)
 		if err != nil {
@@ -69,37 +72,37 @@ func (m *Module) handleDNS(w mdns.ResponseWriter, req *mdns.Msg) {
 	case <-time.After(ServerTimeout): // nothing's ready to go, send empty response
 		// no answer, nothing to send
 	}
-	/*
-		ok := true
-		// opportunistically grab up to 1 more
-		for i := 0; ok && i < 1; i++ { // ten is max number of answers
-			select {
-			case item = <-m.downstreamKCPData:
-				ok = true
-				itemCount++
-				// base32 encode, then dotify / "DNS chop"
-				b32s, err := Dotify(item)
-				if err != nil {
-					events.Error(m.node, err)
-					return
-				}
-				rr := new(mdns.A)
-				rr.Hdr = mdns.RR_Header{Name: b32s, Rrtype: mdns.TypeA, Class: mdns.ClassINET, Ttl: 0}
-				rr.A = net.ParseIP("192.168.1.1") // todo: this should be data
-				answers = append(answers, rr)
-			default:
-				ok = false
-			}
-		}
-	*/
 	msg.Answer = answers
 
-	events.Info(m.node, "handleDNS Server packed answers:", len(answers))
+	maxItemSize := math.Ceil(1.6*float64(MTU)) + 15 // base32 overhead plus 15 bytes per-answer overhead
+	ok := true
+	// opportunistically grab up to 10 more, without exceeding max DNS message length of 512
+	// practically speaking, this will usually only grab up 1 or 2 more
+	for i := 0; ok && i < 10; i++ { // ten is max number of answers
+		if float64(512-msg.Len()) < maxItemSize {
+			break
+		}
 
-	// scrub out the original name to save space, matching transaction ID is all you need anyway
-	msg.Question = make([]mdns.Question, 1)
-	msg.Question[0] = mdns.Question{Name: "mail.", Qtype: req.Question[0].Qtype, Qclass: mdns.ClassINET}
+		select {
+		case item = <-m.downstreamKCPData:
+			ok = true
+			// base32 encode, then dotify / "DNS chop"
+			b32s, err := Dotify(item)
+			if err != nil {
+				events.Error(m.node, err)
+				return
+			}
+			rr := new(mdns.A)
+			rr.Hdr = mdns.RR_Header{Name: b32s, Rrtype: mdns.TypeA, Class: mdns.ClassINET, Ttl: 0}
+			rr.A = net.ParseIP("192.168.1.1") // todo: this should be data
+			answers = append(answers, rr)
+		default:
+			ok = false
+		}
+		msg.Answer = answers
+	}
 
+	events.Info(m.node, "handleDNS Server packed answers:", len(answers), " msg len: ", msg.Len())
 	w.WriteMsg(msg)
 
 	m.debouncerServerUpdate.Trigger()
@@ -111,10 +114,7 @@ func (m *Module) serverUpdate() {
 	m.serverMutex.Lock()
 	n := m.kcpServer.Recv(buffer)
 	m.serverMutex.Unlock()
-	// events.Info(m.node, "kcpServer.Recv read: ", n)
 	if n > 0 {
-		// events.Info(m.node, "kcpServer.Recv got raw: ", buffer[:n], string(buffer[:n]), len(buffer[:n]))
-
 		// handle response
 		am, err := api.RemoteCallFromBytes(&buffer)
 		if err != nil {
